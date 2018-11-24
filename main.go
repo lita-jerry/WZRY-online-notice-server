9ce6250e2909f273a6ce90c2cc6c7f09bb1469f0
Jerry <shengfengzhao@icloud.com>
2018-11-24 20:07:09 +0800

    自定义cookie和token

--- main.go
+++ main.go
@@ -3,28 +3,31 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
-	"time"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
+var token = "SydKGR8N"
+var cookie = "accessToken=16_NxZhosEUeFmVHTOt0h3_lMcKytG5SU4CPZaUdqi1uHpqoh867Jud8djQeapTaxjaf13KDZHqq6Z0ko6o7kxQ8QV0AxsCnI0648K69B2GEeo; appOpenId=oFhrws5IUYTYRF7hnKV_9SYOgbNY"
+
 type Data struct {
-	RoleName	string 	// 昵称
-	GameOnline	int		// 在线状态 0:离线 1:在线 2:正在游戏
-	RoleBigIcon	string 	// 头像
-	JobName		string 	// 段位
-	AllStar 	int 	// 段位升级需要星星的数量
-	RankingStar string 	// 当前星星
-	TotalCount 	int	    // 总场数
-	WinRate		string 	// 胜率
-	MvpNum 		int 	// MVP数量
-	RoleUrl 	string 	// 过往赛季概况的页面url
+	RoleName    string // 昵称
+	GameOnline  int    // 在线状态 0:离线 1:在线 2:正在游戏
+	RoleBigIcon string // 头像
+	JobName     string // 段位
+	AllStar     int    // 段位升级需要星星的数量
+	RankingStar string // 当前星星
+	TotalCount  int    // 总场数
+	WinRate     string // 胜率
+	MvpNum      int    // MVP数量
+	RoleUrl     string // 过往赛季概况的页面url
 }
 
 type ResultData struct {
-	Result 	   int
+	Result     int
 	ReturnCode int
 	ReturnMsg  string
 	Time       string
@@ -48,11 +51,11 @@ func main() {
 
 	for {
 		select {
-		case <- stateChanged:
+		case <-stateChanged:
 			// push
 			fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "stateChanged: ", r.Data.GameOnline)
 
-		case <- getUserStateError:
+		case <-getUserStateError:
 			fmt.Println(time.Now().Format("2006-01-02 15:04:05"), "getUserStateError: ", r.ReturnMsg)
 			return
 		}
@@ -65,7 +68,7 @@ func lestenEventStart(c chan bool, e chan string) {
 
 	for {
 		getUserState()
-		// fmt.Println(r)
+		fmt.Println(r)
 
 		if r.Result != 0 {
 			e <- r.ReturnMsg
@@ -73,25 +76,30 @@ func lestenEventStart(c chan bool, e chan string) {
 		}
 
 		if r.Data.GameOnline != currentState {
-			c <- true 
+			c <- true
 			currentState = r.Data.GameOnline
 		}
 
 		time.Sleep(60 * time.Second)
 	}
-	
+
 }
 
 func getUserState() {
 	url := "https://ssl.kohsocialapp.qq.com:10001/game/rolecard"
 
-	timestamp := strconv.FormatInt(time.Now().UnixNano() / 1e6, 10)
+	timestamp := strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
 	fmt.Println(timestamp)
-	payload := strings.NewReader("apiVersion=4&cChannelId=0&cClientVersionCode=2018092102&cClientVersionName=2.36.102&cCurrentGameId=20001&cDeviceCPU=ARM64&cDeviceId=9c46d1c18fea063ce7ca478d8691e9ca8218914b&cDeviceMem=3134406656&cDeviceModel=iPhone&cDeviceNet=WiFi&cDeviceSP=%E4%B8%AD%E5%9B%BD%E8%81%94%E9%80%9A&cDeviceScreenHeight=736&cDeviceScreenWidth=414&cGameId=20001&cGzip=1&cRand="+timestamp+"&cSystem=ios&cSystemVersionCode=12.1&cSystemVersionName=iOS&friendUserId=&gameId=20001&isMI=0&myRoleId=892934648&platType=ios&roleId=886872615&token=UUwgnsPM&userId=460403972&versioncode=2018092102")
+
+	payload := strings.NewReader("apiVersion=4&cChannelId=0&cClientVersionCode=2018092102&cClientVersionName=2.36.102&cCurrentGameId=20001&cDeviceCPU=ARM64&cDeviceId=9c46d1c18fea063ce7ca478d8691e9ca8218914b&cDeviceMem=3134406656&cDeviceModel=iPhone&cDeviceNet=WiFi&cDeviceSP=%E4%B8%AD%E5%9B%BD%E8%81%94%E9%80%9A&cDeviceScreenHeight=736&cDeviceScreenWidth=414&cGameId=20001&cGzip=1" +
+		"&cRand=" + timestamp +
+		"&cSystem=ios&cSystemVersionCode=12.1&cSystemVersionName=iOS&friendUserId=&gameId=20001&isMI=0&myRoleId=892934648&platType=ios&roleId=886872615" +
+		"&token=" + token +
+		"&userId=460403972&versioncode=2018092102")
 
 	req, _ := http.NewRequest("POST", url, payload)
 
-	req.Header.Add("cookie", "accessToken=15_JYoFMFjsUFK5lwjorQYbyCVstof6kBtkmjfu6xsUX5Di4--gHEcRkzDSz2JTFN9p9TcScZF8prPpqd-mT0AaE_cDRsHqv8WTnQeMuGRFINc; appOpenId=oFhrws5IUYTYRF7hnKV_9SYOgbNY")
+	req.Header.Add("cookie", cookie)
 	req.Header.Add("host", "ssl.kohsocialapp.qq.com:10001")
 	req.Header.Add("content-type", "application/x-www-form-urlencoded")
 	// req.Header.Add("accept-encoding", "br, gzip, deflate") // 没有解压, 有乱码
